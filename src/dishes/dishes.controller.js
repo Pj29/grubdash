99a7bdd6fecff2ec048eec8a96351d375144548a
Peter Jones <pjxcvi@proton.me>
2023-09-13 18:34:45 -0400

    update crudl functions in dishes controller

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,4 +6,32 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
+function list(req, res, next) {
+  res.json({ data: dishes });
+}
+
+function create(req, res, next) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newOrder = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function read(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  if (foundDish) {
+    res.json({ data: foundDish });
+  } else {
+    next({
+      status: 404,
+      message: `Dish id not found: ${dishId}`,
+    });
+  }
+}
