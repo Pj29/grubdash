7f7e86e9b2ac8ccb4193baa141e902559ec0474d
Peter Jones <pjxcvi@proton.me>
2023-09-13 19:27:28 -0400

    update crudl functions in dishes controller

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -35,3 +35,39 @@ function read(req, res, next) {
     });
   }
 }
+
+function update(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+
+  if (foundDish) {
+    const { data: { name, description, price, image_url } = {} } = req.body;
+
+    foundDish.name = name;
+    foundDish.description = description;
+    foundDish.price = price;
+    foundDish.image_url = image_url;
+
+    res.json({ data: foundDish });
+  } else {
+    next({
+      status: 404,
+      message: `Dish id not found: ${dishId}`,
+    });
+  }
+}
+
+function destroy(req, res, next) {
+  const { dishId } = req.params;
+  const index = dishes.findIndex((dish) => dish.id === dishId);
+
+  if (index !== -1) {
+    dishes.splice(index, 1);
+    res.sendStatus(204);
+  } else {
+    next({
+      status: 404,
+      message: `Dish id not found: ${dishId}`,
+    });
+  }
+}
